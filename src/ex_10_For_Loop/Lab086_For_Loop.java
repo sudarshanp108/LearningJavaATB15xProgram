643b486515f8bd02155c6fcf4722c811a9dd0ac7
sudarshanp108 <patilsd1997@gmail.com>
2026-02-26 00:13:24 +0530

    F25 practice programs for: Function and Methods in Java

---
+++ src/ex_10_For_Loop/Lab086_For_Loop.java
@@ -0,0 +1,11 @@
+package ex_10_For_Loop;
+
+public class Lab086_For_Loop {
+    static void main(String[] args) {
+        // For Loop
+        // Help to repeat a block of code.
+        // Initialization -> Condition -> Updation (Incre/decre) (ICU)
+        for (int i=0; i<10; i++)
+            System.out.println(i);
+    }
+}
